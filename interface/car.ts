5ae5e6878d8fe972de52f4f3569249201af845dc
krebou <krebou@MacBook-Pro-krebou.local>
2022-08-02 18:00:48 +0200

    first step

---
+++ interface/car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+    id: string;
+    plate: string;
+    mark: string;
+    model: string;
+    vin: string;
+    year: string;
+    client_id: string;
+    createAt?: string;
+    updatedAt?: string;
+}
--- services/CarService/src/services/getCar.ts
+++ services/CarService/src/services/getCar.ts
@@ -1,12 +1,46 @@
 import * as grpc from '@grpc/grpc-js';
-import { GetCarRequest, GetCarResponse } from '../../../../grpc/Car/Car_pb';
+import { CarSchema, GetCarRequest, GetCarResponse } from '../../../../grpc/Car/Car_pb';
+import { MongoDb } from '../../../../middleware/Mongodb';
+import { fromJsonToGrpc } from '../../../../helpers/grpc';
+import { Car } from '../../../../interface/car';
+import { ObjectId } from 'mongodb';
 
 type Call = grpc.ServerUnaryCall<GetCarRequest, GetCarResponse>;
 type Callback = grpc.sendUnaryData<GetCarResponse>;
 
-export const getCar = () => {
-    //Promise<( call: any, callback: any): Promise<void> >
+export const getCar = (mongodb: MongoDb) => {
     return async ({ request }: Call, callback: Callback): Promise<void> => {
 
+        try {
+            const carId = new ObjectId(request.getId());
+
+            //GET FROM DATABASE
+            const carObject = await mongodb.collection.findOne(carId);
+
+            if (carObject === null) {
+                /* SEND RESPONSE_ERROR [GET_CAR] */
+                callback({
+                    code: grpc.status.NOT_FOUND,
+                    message: "Object isn't exist.",
+                });
+            } else {
+                /**
+                 * SUCCESS RESPONSE GRPC
+                 */
+                const carSchema = fromJsonToGrpc<CarSchema, Car>(new CarSchema(), carObject, {
+                    getTimeChange: true,
+                });
+
+                const responseGRPC = new GetCarResponse();
+                responseGRPC.setCar(carSchema);
+
+                callback(null, responseGRPC);
+            }
+
+            //
+        } catch (e) {
+            /* SEND RESPONSE_ERROR [GET_CAR] */
+            callback(e, null);
+        }
     };
 };
