e932db9da68484aec3a24fc563570cad55ea6cf8
Bartosz Chwarscianek <bartosz@chwarscianek.pl>
2022-11-10 15:23:07 +0100

    add https apiService

--- services/ApiService/src/index.ts
+++ services/ApiService/src/index.ts
@@ -1,3 +1,14 @@
 import { App } from './app';
+import * as https from 'node:https';
+import * as fs from 'node:fs';
 
-App.listen(3000);
+if (process.env.NODE !== 'production') {
+    App.listen(3000);
+} else {
+    const options = {
+        key: fs.readFileSync('/core/cert/hoa.entmor.de/privkey1.pem'),
+        cert: fs.readFileSync('/core/cert/hoa.entmor.de/fullchain1.pem'),
+    };
+
+    https.createServer(options, App).listen(3000);
+}
